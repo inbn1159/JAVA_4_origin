b2a8fd2ce9c2670c8dc0b3a23c9ef672e7824395
inbn1159 <inbn1159@gmail.com>
2023-11-22 14:45:07 +0900

    크롤링 한 데이터로 검색 기능 재설정

--- bookbook/src/main/java/org/bookbook/controller/BookController.java
+++ bookbook/src/main/java/org/bookbook/controller/BookController.java
@@ -1,11 +1,15 @@
 package org.bookbook.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.bookbook.domain.BookSearchVO;
-import org.bookbook.domain.CategoriesVO;
+import org.bookbook.domain.GenreVO;
+import org.bookbook.domain.TopicVO;
 import org.bookbook.service.BookSearchService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,17 +29,72 @@ public class BookController {
     BookSearchService service;
 
     @ModelAttribute("searchBook")
-    public JSONObject searchBookTypes(CategoriesVO vo) {
-        List<CategoriesVO> cate = service.getCategoriesList(vo);
+    public JSONObject searchBookTypes(TopicVO topics, GenreVO genres) {
+        List<TopicVO> topicList = service.getTopicList(topics);
 
-        Map<String, String> map = new LinkedHashMap<>();
+        List<GenreVO> genreList = service.getGenreList(genres);
 
-        for (CategoriesVO category : cate) {
-            map.put(category.getGenre(), category.getCategories());
+        Map<String, List<String>> genreConvertedMap = convertToMap(genreList);
+
+        // log.info(genreConvertedMap);
+
+        Map<String, Map<String, List<String>>> map = new LinkedHashMap<>();
+
+        // log.info(topicList);
+
+        for (TopicVO topic : topicList) {
+            Map<String, List<String>> genreMap = new LinkedHashMap<>();
+
+            String genreToString = topic.getGenres();
+
+            // 초기 용량 설정을 통한 성능 최적화
+            List<String> genreToList = new ArrayList<>(Arrays.asList(genreToString.split(", ")));
+
+            for (String genre : genreToList) {
+                // 불필요한 객체 생성 최적화
+                // genre = genre.trim();
+                log.info("----->---->" + genre);
+                List<String> categoriesToList = genreConvertedMap.get(genre);
+
+                log.info("----------------->" + categoriesToList);
+
+                // 이미 있는 리스트를 재활용하여 새로운 리스트를 생성하지 않도록 최적화
+                if (categoriesToList == null) {
+                    categoriesToList = new ArrayList<>();
+                }
+
+                genreMap.put(genre, categoriesToList);
+            }
+
+            map.put(topic.getTopic(), genreMap);
         }
+
         JSONObject jsonObject = new JSONObject(map);
 
         return jsonObject;
+    }
+
+    public static Map<String, List<String>> convertToMap(List<GenreVO> genreList) {
+        Map<String, List<String>> genreMap = new HashMap<>();
+
+        for (GenreVO genreVO : genreList) {
+            String genre = genreVO.getGenre();
+            String categoriesToString = genreVO.getCategories();
+
+            List<String> categoriesList = new ArrayList<>();
+
+            if (categoriesToString != null) {
+                categoriesList = new ArrayList<String>(Arrays.asList(categoriesToString.split(", ")));
+            }
+
+            // log.info("------->>>"+categoriesList);
+
+            genreMap.put(genre, categoriesList);
+        }
+
+        // log.info("--------------------->>>>>"+ genreMap);
+
+        return genreMap;
     }
 
     @GetMapping("/list")
--- bookbook/src/main/java/org/bookbook/mapper/BookSearchMapper.java
+++ bookbook/src/main/java/org/bookbook/mapper/BookSearchMapper.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.bookbook.domain.BookSearchVO;
 import org.bookbook.domain.BookVO;
-import org.bookbook.domain.CategoriesVO;
+import org.bookbook.domain.GenreVO;
+import org.bookbook.domain.TopicVO;
 
 @Mapper
 public interface BookSearchMapper {
 
 	public List<BookVO> read(BookSearchVO bookSearch);
-	public List<CategoriesVO> get(CategoriesVO categories);
+	
+	public List<TopicVO> getTopic(TopicVO topic);
+	
+	public List<GenreVO> getGenre(GenreVO genre);
 }
