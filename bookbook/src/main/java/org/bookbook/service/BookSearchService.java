9a7d916a808ee5c4e9d69943fa087991fa924576
HyeonsungLee <sss23236@gmail.com>
2023-11-23 16:21:03 +0900

    카테고리 결과 페이지 카드형식 이미지 나열, 페이징 처리 기능 구현

--- bookbook/src/main/java/org/bookbook/service/BookSearchService.java
+++ bookbook/src/main/java/org/bookbook/service/BookSearchService.java
@@ -6,11 +6,17 @@ import org.bookbook.domain.BookSearchVO;
 import org.bookbook.domain.BookVO;
 import org.bookbook.domain.GenreVO;
 import org.bookbook.domain.TopicVO;
+import org.bookbook.model.Criteria;
 
 public interface BookSearchService {
-    public List<BookVO> getBookList(BookSearchVO bookSearch);
+	public List<BookVO> getBookList(BookSearchVO bookSearch);
 
-    public List<TopicVO> getTopicList(TopicVO topic);
+	public List<TopicVO> getTopicList(TopicVO topic);
 
-    public List<GenreVO> getGenreList(GenreVO genre);
+	public List<GenreVO> getGenreList(GenreVO genre);
+
+	public List<BookVO> getListPaging(Criteria cri);
+	
+	 /* 게시판 총 갯수 */
+    public int getTotal();
 }
